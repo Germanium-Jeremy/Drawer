bab64682542f79328bc5d44a9d3b0c1733c459ea
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-06-06 17:06:30 +0200

    compile button instead of a 3 seconds debounce

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -7,7 +7,7 @@ import { useDraw } from "../contexts/DrawContext"
 function SideMenu() {
     const { user, openLogin } = useAuth()
     const { openFileSelect } = useFile()
-    const { commands, handleSetCommands } = useDraw()
+    const { commands, handleSetCommands, handleGenerateDiagram } = useDraw()
 
     const handleFileClick = () => {
         if (!user) {
@@ -26,10 +26,13 @@ function SideMenu() {
 
             <div className="flex flex-col gap-4 h-full mb-10">
                 <h1 className="text-lg font-bold">Paste or write your codes here.</h1>
-                <div className="bg-white p-4 rounded-md shadow-md h-full border-2 border-gray-300 focus-within:border-amber-500">
-                    <textarea className="w-full h-full focus:outline-none resize-none" placeholder="Write your code here..."
+                <div className="bg-white p-4 rounded-md shadow-md h-full border-2 border-gray-300 focus-within:border-amber-500 flex flex-col">
+                    <textarea className="w-full flex-grow focus:outline-none resize-none" placeholder="Write your code here..."
                         value={commands} onChange={(e) => handleSetCommands(e.target.value)}
                     ></textarea>
+                    <button className="mt-2 bg-amber-500 hover:bg-amber-600 text-white font-semibold py-1 px-3 rounded"
+                        onClick={() => handleGenerateDiagram()}
+                    >Compile</button>
                 </div>
             </div>
 
