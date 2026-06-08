6589ff81bc4bb7211c46a5a5a1f9d0618eed6e94
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-06-08 10:51:19 +0200

    Side menu for codes inputs

---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,45 @@
+import { FaFile } from "react-icons/fa"
+import { useAuth } from "../contexts/AuthContext"
+import { useFile } from "../contexts/FileContext"
+import { TrancateText } from '../lib/common'
+
+function SideMenu() {
+    const { user, openLogin } = useAuth()
+    const { openFileSelect } = useFile()
+
+    const handleFileClick = () => {
+        if (!user) {
+            openLogin()
+        } else {
+            openFileSelect()
+        }
+    }
+
+    return (
+        <div className="w-100 bg-mist-300 h-full px-8 py-4 flex flex-col gap-4 border-r border-gray-800">
+            <div className="flex items-center gap-4 mb-6">
+                <FaFile className='text-xl text-gray-800 cursor-pointer' onClick={handleFileClick} />
+                <h1 className="text-2xl font-bold bg-linear-to-r from-amber-500 to-gray-500 bg-clip-text text-transparent">Drawer</h1>
+            </div>
+
+            <div className="flex flex-col gap-4 h-full mb-10">
+                <h1 className="text-lg font-bold">Paste or write your codes here.</h1>
+                <div className="bg-white p-4 rounded-md shadow-md h-full border-2 border-gray-300 focus-within:border-amber-500">
+                    <textarea className="w-full h-full focus:outline-none resize-none" placeholder="Write your code here..."></textarea>
+                </div>
+            </div>
+
+            <hr className="border-gray-800 border" />
+
+            <div className="flex items-center gap-2">
+                <img src="/favicon.svg" alt="Preview" className="w-12 h-12 rounded-full shadow-md border-2 border-amber-500 bg-white" />
+                <div className="flex flex-col">
+                    <h2 className='font-semibold text-lg'>{user?.username || "username"}</h2>
+                    <h2>{TrancateText(user?.email || "email@example.com", 23)}</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SideMenu
