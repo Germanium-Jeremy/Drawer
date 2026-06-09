f4e6a6860687d220c1da7055820d96a1fbd4ad6a
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-06-09 10:42:41 +0200

    refactored to remove redundant codes logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,20 @@ import MainWindow from './components/MainWindow'
 import AuthDialog from './components/AuthDialog'
 import SignupDialog from './components/SignupDialog'
 import FileSelectionDialog from './components/FileSeletion'
-import { useState } from 'react'
 import { useAuth } from './contexts/AuthContext'
+import { useFile } from './contexts/FileContext'
 
 export default function App() {
-    const [showLogin, setShowLogin] = useState(false)
-    const [showRegister, setShowRegister] = useState(false)
-    const [showFileSelect, setShowFileSelect] = useState(false)
-    const { user } = useAuth()
-
-    const handleFileDisplay = () => {
-        if (!user) {
-            handleLoginDisplay()
-        } else {
-            setShowFileSelect(!showFileSelect)
-        }
-    }
-
-    const handleLoginDisplay = () => {
-        setShowLogin(!showLogin)
-    }
-    
-    const handleRegisterDisplay = () => {
-        setShowRegister(!showRegister)
-    }
+    const { user, showLogin, showRegister } = useAuth()
+    const { showFileSelect } = useFile()
 
     return (
         <div className="flex items-center justify-center h-screen">
-            <SideMenu handleFileShow={handleFileDisplay} />
-            <MainWindow handleLoginShow={handleLoginDisplay} handleRegisterShow={handleRegisterDisplay} />
-            {!user && showLogin && <AuthDialog handleHide={handleLoginDisplay} />}
-            {!user && showRegister && <SignupDialog handleHide={handleRegisterDisplay} />}
-            {user && showFileSelect && <FileSelectionDialog handleHide={handleFileDisplay} />}
+            <SideMenu />
+            <MainWindow />
+            {!user && showLogin && <AuthDialog />}
+            {!user && showRegister && <SignupDialog />}
+            {user && showFileSelect && <FileSelectionDialog />}
         </div>
     )
 }
